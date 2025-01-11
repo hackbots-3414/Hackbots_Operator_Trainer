50a8848a4ba2c234017bf8b4ce66f1e037a44f13
Minejerik <jaanerikf@gmail.com>
2025-01-11 16:02:50 -0500

    Finished controller connection check, working on actually starting

--- intro_scene.gd
+++ intro_scene.gd
@@ -8,4 +8,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var controllers = Input.get_connected_joypads()
+	var controller_label:Label = $VBoxContainer/HBoxContainer2/controller_connect_text
+	var controller_settings = controller_label.label_settings
+	
+	if len(controllers) > 0:
+		controller_settings.font_color = Color("#00FF00")
+		controller_label.text = "Controller Connected!"
+	else:
+		controller_settings.font_color = Color("#FF0000")
+		controller_label.text = "No Controller Connected!"
+
+
+func _start_button_pressed() -> void:
+	pass # Replace with function body.
